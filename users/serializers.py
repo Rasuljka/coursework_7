42c4db0aa6b7b891c1fadaf05ece02be4b8f862d
Rasuljka <ras199167@gmail.com>
2024-07-06 01:12:02 +0500

    fix 2

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from users.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
